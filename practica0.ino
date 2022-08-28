c90bf982a2c0cf8ba4e1e6098f2cfbe1a6e4fb69
Bryan Contreras <bryan37saggio@gmail.com>
2022-08-27 21:48:58 -0500

    Agregamos una comparación para encender la bomba de agua

--- practica0.ino
+++ practica0.ino
@@ -10,6 +10,18 @@ void setup ()
 
 void loop ()
 {
-
+int SensorNivel = digitalRead(nivel); //leemos lo que marca el nivel
+  
+   if (SensorNivel==1) 
+  {
+   Serial.println("Nivel de agua correcto, se puede regar");
+   digitalWrite(bomba, HIGH);
+   delay(3000);
+   digitalWrite(bomba, LOW);
+   delay(500);
+   }
+    if (SensorNivel==0)
+  { Serial.println(" Nivel bajo de Agua. Rellenar el tanque"); 
+  delay(1000);}
   
 }
